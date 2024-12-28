879f4897d7396b058dd0e0b97d094892ddcf697d
vgershun <vgershun@gmail.com>
2024-12-28 13:48:42 +0300

    colored printing

---
+++ colors.c
@@ -0,0 +1,31 @@
+/* Copyright (c) 2024 by valixxx1
+ * License - MIT
+*/
+
+void red(char* text){
+	printf("\033[31m%s\033[0m", text);
+}
+
+void orange(char* text){
+	printf("\033[33m%s\033[0m", text);
+}
+
+void yellow(char* text){
+	printf("\033[93m%s\033[0m", text);
+}
+
+void green(char* text){
+	printf("\033[32m%s\033[0m", text);
+}
+
+void lightblue(char* text){
+	printf("\033[36m%s\033[0m", text);
+}
+
+void blue(char* text){
+	printf("\033[34m%s\033[0m", text);
+}
+
+void purple(char* text){
+	printf("\033[35m%s\033[0m", text);
+}
---
+++ colors.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2024 by valixxx1
+ * License - MIT
+*/
+
+#ifndef COLORS_H
+#define COLORS_H
+
+void red(char *text);
+void orange(char *text);
+void yellow(char *text);
+void green(char *text);
+void lightblue(char *text);
+void blue(char *text);
+void purple(char *text);
+
+#endif
